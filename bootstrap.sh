a07b7c23acdd44f4a4edfa096d3c76b361b39ce8
Steven Skeard <stevenskeard@gmail.com>
2019-05-16 23:51:01 -0400

    Fix android sdk config

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ set -e
 # Setup vars
 export VAGRANT_HOME="/home/vagrant"
 export ANDROID_HOME="/opt/android-sdk-linux"
-export SDK_TOOLS_VERSION="26.1.1"
+export SDK_TOOLS_VERSION="25.2.5"
 export API_LEVELS="android-23,android-24,android-25,android-26,android-27,android-28"
 export BUILD_TOOLS_VERSIONS="build-tools-28.0.3"
 export ANDROID_EXTRAS="extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository"
@@ -28,12 +28,12 @@ sudo apt-get install -y \
     'gradle'
 
 # Install android
-mkdir -p /opt/android-sdk-linux && cd /opt \
-    && wget -q http://dl.google.com/android/repository/tools_r${SDK_TOOLS_VERSION}-linux.zip -O android-sdk-tools.zip \
-    && unzip -q -o android-sdk-tools.zip -d ${ANDROID_HOME} \
-    && rm -f android-sdk-tools.zip \
-    && echo y | android update sdk --no-ui -a --filter \
-       tools,platform-tools,${ANDROID_EXTRAS},${API_LEVELS},${BUILD_TOOLS_VERSIONS} --no-https
+mkdir -p ${ANDROID_HOME}
+cd /tmp
+wget -q http://dl-ssl.google.com/android/repository/tools_r${SDK_TOOLS_VERSION}-linux.zip -O android-sdk-tools.zip
+unzip -qq -o android-sdk-tools.zip -d ${ANDROID_HOME}
+rm -f android-sdk-tools.zip
+echo y | android update sdk --no-ui -a --filter tools,platform-tools,${ANDROID_EXTRAS},${API_LEVELS},${BUILD_TOOLS_VERSIONS} --no-https
 
 # Install required NPM packages
 sudo npm install -g ionic cordova
